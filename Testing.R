266710a2e4a1c901b3ee5e544a095f26d3a6c4cd
This Soft <solokodzo@gmail.com>
2023-04-21 19:13:53 +0000

    Innitial Commit on Testing.R

---
+++ Testing.R
@@ -0,0 +1,27 @@
+
+R version 4.2.3 (2023-03-15 ucrt) -- "Shortstop Beagle"
+Copyright (C) 2023 The R Foundation for Statistical Computing
+Platform: x86_64-w64-mingw32/x64 (64-bit)
+
+R is free software and comes with ABSOLUTELY NO WARRANTY.
+You are welcome to redistribute it under certain conditions.
+Type 'license()' or 'licence()' for distribution details.
+
+  Natural language support but running in an English locale
+
+R is a collaborative project with many contributors.
+Type 'contributors()' for more information and
+'citation()' on how to cite R or R packages in publications.
+
+Type 'demo()' for some demos, 'help()' for on-line help, or
+'help.start()' for an HTML browser interface to help.
+Type 'q()' to quit R.
+
+[Previously saved workspace restored]
+
+> print("This file was created within RStudio")
+[1] "This file was created within RStudio"
+> 
+> print("And now it lives on GitHub")
+[1] "And now it lives on GitHub"
+> 
